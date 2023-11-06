970d898583393816aefb124e3c156b4c868b5f43
Katherine Shashkina <kshashkina@kse.org.ua>
2023-11-06 08:18:05 +0200

    encryption function

---
+++ main.py
@@ -0,0 +1,13 @@
+def caesar_encrypt(text, key):
+    encrypted_text = ""
+    for char in text:
+        if char.isalpha():
+            key_value = (ord(char) - ord('A') if char.isupper() else ord(char) - ord('a')) + key
+            if char.isupper():
+                encrypted_text += chr(((key_value % 26) + ord('A')))
+            else:
+                encrypted_text += chr(((key_value % 26) + ord('a')))
+        else:
+            encrypted_text += char
+    return encrypted_text
+
