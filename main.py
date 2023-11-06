95e22e58a75cdd8317d7bb5ece249d414715b2eb
Katherine Shashkina <kshashkina@kse.org.ua>
2023-11-06 09:09:35 +0200

    reader and writer functions + their usage in main

--- main.py
+++ main.py
@@ -16,17 +16,36 @@ def caesar_decrypt(text, key):
     return caesar_encrypt(text, -key)
 
 
+
+def readFile(path):
+    with open(path, 'r') as file:
+        return file.read()
+
+
+def writeFile(path, text):
+    with open(path, 'w') as file:
+        file.write(text)
+
+
 def main():
-    text = input("Enter the text: ")
+ while True:
+    command = int(input("Choose your command\n1 - Encrypt text into the file\n2 - decrypt text from the file\n"))
+    result_text = ""
+
     key = int(input("Enter the key value: "))
+    read = input("Enter the name of the input file: ").strip()
+    write = input("Enter the name of the output file: ").strip()
+    text = readFile(read)
 
-    encrypted_text = caesar_encrypt(text, key)
-    decrypted_text = caesar_decrypt(encrypted_text, key)
+    if command == 1:
+        result_text = caesar_encrypt(text, key)
+    elif command == 2:
+        result_text = caesar_decrypt(text, key)
+    else:
+        print("Invalid input")
+        return
 
-    print("Original text:", text)
-    print("Encrypted text:", encrypted_text)
-    print("Decrypted text:", decrypted_text)
-
+    writeFile(write, result_text)
 
 if __name__ == "__main__":
     main()
